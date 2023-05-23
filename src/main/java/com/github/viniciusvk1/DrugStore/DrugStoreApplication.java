2bda8332023af90ad6f213776568fe0e2333d043
Vinicius <viniciusalmeida.vk1@gmail.com>
2023-05-23 09:14:16 -0300

    initial file

---
+++ src/main/java/com/github/viniciusvk1/DrugStore/DrugStoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.viniciusvk1.DrugStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/viniciusvk1/DrugStore/DrugStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.viniciusvk1.DrugStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
